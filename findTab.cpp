f28da8347c09dfa70752830281fa6f3509b77eb4
stoyanov-st <st.stoyanov04@gmail.com>
2016-09-18 15:51:01 +0300

    Initial commit

---
+++ findTab.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+Pointer findTab(Pointer p, string sym, Attribute attr)
+{
+	if(!p)
+	{
+		this->makeItem(p, sym, attr);
+	}
+	else if(sym == this->sym)
+	{
+		return p;
+	}
+	else if(sym < this->sym)
+	{
+		return findTab(p->left, sym, attr);
+	}
+	else return findTab(p->right, sym, attr);
+}
